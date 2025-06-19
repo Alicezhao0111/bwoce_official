9315b5ddc701fa8ab86be216386e2c648190de57
Alice Zhao <goodchild40723@gmail.com>
2025-06-19 17:25:15 +0800

    circle_main processing

---
+++ js/circle.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+  new Swiper('.circle-page__carousel', {
+    loop: true,
+    pagination: {
+      el: '.circle-page__pagination',
+      clickable: true,
+    },
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+   new Swiper("#circle-event-swiper", {
+    slidesPerView: 3,
+    slidesPerGroup: 1,
+    loop: true,
+    spaceBetween: 8,
+    navigation: {
+      prevEl: "#circle-event-swiper .circle-section__nav--prev",
+      nextEl: "#circle-event-swiper .circle-section__nav--next",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: "auto",
+        slidesPerGroup: 1,
+        centeredSlides: false,
+        spaceBetween: 4,
+        slidesOffsetBefore: 0,
+      },
+      1120: {
+        slidesOffsetBefore: 8,
+      }
+      
+
+    },
+  });
+});
+
+
